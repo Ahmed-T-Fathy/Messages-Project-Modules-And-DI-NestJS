e9f0d8072060e129208f7c8a542c64cb463c1da7
tarek-watanya <tarekwatanya@gmail.com>
2024-05-30 12:37:12 +0300

    first commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post,Body,Param } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {}
+
+  @Post()
+  createMessage(@Body() body:any) {
+    console.log(body.content);
+    
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id:any) {
+
+  }
+}
