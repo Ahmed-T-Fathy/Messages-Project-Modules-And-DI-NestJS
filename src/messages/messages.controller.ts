408ce1ce06e68b4cc77a44017a379121de95ab7b
tarek-watanya <tarekwatanya@gmail.com>
2024-06-02 00:30:24 +0300

    understanding of how Modules and Dependency Injection work together

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,19 +1,23 @@
 import { Controller, Get, Post,Body,Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+
+  constructor(public messagesService:MessagesService){}
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body:CreateMessageDto) {
-    console.log(body.content);
-    
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id:any) {
-
+    return this.messagesService.findOne(id);
   }
 }
